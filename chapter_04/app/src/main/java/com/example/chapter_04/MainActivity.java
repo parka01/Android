1a582fc8b4488e5617e51ed48a8c594a1e767843
parka01 <68191916+parka01@users.noreply.github.com>
2021-03-05 13:47:45 +0900

    3/5 Android activity

--- chapter_04/app/src/main/java/com/example/chapter_04/MainActivity.java
+++ chapter_04/app/src/main/java/com/example/chapter_04/MainActivity.java
@@ -10,8 +10,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.ex04_01);
-        setContentView(R.layout.ex04_02);
+        //setContentView(R.layout.ex04_02);
         //setContentView(R.layout.ex04_03);
         //setContentView(R.layout.ex04_04);
+        //setContentView(R.layout.ex04_05);
+        //setContentView(R.layout.ex04_06);
+        //setContentView(R.layout.ex04_07);
+        //setContentView(R.layout.ex04_08);
+        //setContentView(R.layout.ex04_09);
+        //setContentView(R.layout.ex04_10);
+        //setContentView(R.layout.ex04_11);
+        //setContentView(R.layout.ex04_12);
+        //setContentView(R.layout.ex04_13);
+        //setContentView(R.layout.ex04_14);
+        //setContentView(R.layout.ex04_15);
+        //setContentView(R.layout.ex04_16);
     }
 }
